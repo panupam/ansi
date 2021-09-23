3acf81eaf478ae5c1162b43f2514d77982ed0ce8
Pranav K Anupam <pranavanupam@yahoo.com>
2021-09-23 10:50:09 +0530

    initial commit

---
+++ binom.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int n,N,i;
+	float p;
+	printf("Enter No of sample to be generated : ");
+	scanf("%d",&N);
+	printf("Enter the parameter (n & p) for generating Binomial Distribution : ");
+	scanf("%d%f",&n,&p);
+
+	
+}
